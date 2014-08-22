2b99d98987163a3efca1c2596a557faf87635b4a
Ricium <rm.awsum@gmail.com>
2014-08-22 09:10:50 +0200

    Added Database Connection & Database Restore

---
+++ SparStelsel/App_Code_/AutoLogOffActionFilterAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SparStelsel
+{
+    public class AutoLogOffActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            HttpContextBase ctx = filterContext.HttpContext;
+            if (ctx.Session["UserID"] == null)
+            {
+                ctx.Session.Abandon();
+                filterContext.Result = OnSessionExpiryRedirectResult(filterContext);
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public virtual ActionResult OnSessionExpiryRedirectResult(ActionExecutingContext filterContext)
+        {
+            return new RedirectToRouteResult(new RouteValueDictionary{
+                    { "controller", "Account" },
+                    { "action", "LogOn" },
+                    { "ReturnUrl", filterContext.HttpContext.Request.Url.PathAndQuery }
+                });
+        }
+
+    }
+}
---
+++ SparStelsel/App_Code_/DatabaseConnection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SparStelsel
+{
+    public class DataBaseConnection
+    {
+        public SqlConnection SqlConn()
+        {
+            //Defines which SQL-server to connect to, which database, and which user
+            string strConn = "";
+
+            foreach (ConnectionStringSettings item in ConfigurationManager.ConnectionStrings)
+            {
+                if (item.Name.Equals("SparConnect"))
+                {
+                    strConn = item.ConnectionString;
+                }
+            }
+
+            SqlConnection con = new SqlConnection(strConn);
+            return con;
+        }
+
+        public SqlConnection AuthConn()
+        {
+            //Defines which SQL-server to connect to, which database, and which user
+            string strConn = "";
+
+            foreach (ConnectionStringSettings item in ConfigurationManager.ConnectionStrings)
+            {
+                if (item.Name.Equals("SparConnect"))
+                {
+                    strConn = item.ConnectionString;
+                }
+            }
+
+            SqlConnection con = new SqlConnection(strConn);
+            return con;
+        }
+    }
+}

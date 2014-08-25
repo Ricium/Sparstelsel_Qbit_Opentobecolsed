630e196462999a3984cebf28ad3a8a392163d348
Ricium <rm.awsum@gmail.com>
2014-08-25 18:27:47 +0200

    Added Stored Procedures class

---
+++ SparStelsel/App_Code_/StoredProcedures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SparStelsel
+{
+    /// <summary>
+    /// Stored Procedures is a collection of static readonly strings assosiated with the Stored Procedures in the Database.
+    /// This is to allow the developers to only change a single member to propagate across the project. 
+    /// </summary>
+    public class StoredProcedures
+    {       
+        public static readonly string InsertUser = "Insert_User";
+    }
+}

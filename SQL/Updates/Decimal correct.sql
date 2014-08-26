634e6ba8dd7b3d391dfe94c0dea7227a6662be38
bosvark8 <jjduplooy@yahoo.com>
2014-08-26 08:27:19 +0200

    Created transit model and added database correction

---
+++ SQL/Updates/Decimal correct.sql
@@ -0,0 +1,2 @@
+alter table l_Transit
+alter column Amount decimal(18, 2)
---
+++ SparStelsel/Models/Transit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SparStelsel.Models
+{
+    /// <summary>
+    /// Transit model links to l_Transit. 
+    /// Transit table is used to save transit
+    /// 
+    /// Used by: FK
+    /// 
+    /// Uses: Sy eie FK
+    /// </summary>
+    public class Transit
+    {
+        [DisplayName("Transit ID")]
+        [Required(ErrorMessage = "Transit ID is required.")]
+        //Vir varchar sit hierdie in: 
+        //[StringLength(10,ErrorMessage = "Transit ID may not be longer than 10 characters")]
+        public int TransitID { get; set; }
+
+        [DisplayName("Date")]
+        [Required(ErrorMessage = "Date is required.")]
+        public DateTime ActualDate { get; set; }
+
+        [DisplayName("Modified Date")]
+        public DateTime ModifiedDate { get; set; }
+
+        [DisplayName("Amount")]
+        [Required(ErrorMessage = "Amount is required.")]
+        public decimal Amount { get; set; }
+    }
+}

853ca2b9d8a02764d65ad194dd0e0819efd690f9
bosvark8 <jjduplooy@yahoo.com>
2014-09-19 22:08:33 +0200

    Repositories created: Kwikpaytype and MoneyUnit.
    Wil net bevsetig eks op die regte pad. Gaan more voort met die proses.

--- SparStelsel/Models/KwikPayTypeRepository.cs
+++ SparStelsel/Models/KwikPayTypeRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,205 @@ namespace SparStelsel.Models
 {
     public class KwikPayTypeRepository
     {
+        public KwikPayType GetKwikPayType(int KwikPayTypeID)
+        {
+            //...Create New Instance of Object...
+            KwikPayType ins = new KwikPayType();
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            SqlCommand cmdI;
+
+            //...SQL Commands...
+            cmdI = new SqlCommand("SELECT * FROM l_KwikPayType WHERE KwikPayTypeID =" + KwikPayTypeID, con);
+            cmdI.Connection.Open();
+            SqlDataReader drI = cmdI.ExecuteReader();
+
+            //...Retrieve Data...
+            if (drI.HasRows)
+            {
+                while (drI.Read())
+                {
+                    ins.KwikPayTypeID = Convert.ToInt32(drI["KwikPayTypeID"]);
+                    ins.KwikPayType = Convert.ToChar(drI["KwikPayType"]);
+                }
+            }
+
+            //...Close Connections...
+            drI.Close();
+            con.Close();
+
+
+            //...Return...
+            return ins;
+        }
+
+        public List<KwikPayType> GetAllKwikPayType()
+        {
+            //...Create New Instance of Object...
+            List<KwikPayType> list = new List<KwikPayType>();
+            KwikPayType ins;
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            SqlCommand cmdI;
+
+            //...SQL Commands...
+            cmdI = new SqlCommand("SELECT * FROM l_KwikPayType", con);
+            cmdI.Connection.Open();
+            SqlDataReader drI = cmdI.ExecuteReader();
+
+            //...Retrieve Data...
+            if (drI.HasRows)
+            {
+                while (drI.Read())
+                {
+                    ins = new KwikPayType();
+                    ins.KwikPayTypeID = Convert.ToInt32(drI["KwikPayTypeID"]);
+                    ins.KwikPayType = Convert.ToChar(drI["KwikPayType"]);
+                    list.Add(ins);
+                }
+            }
+
+            //...Close Connections...
+            drI.Close();
+            con.Close();
+
+
+            //...Return...
+            return list;
+        }
+
+        public KwikPayType Insert(KwikPayType ins)
+        {
+            //...Get User and Date Data...
+            // string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            // int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+            // string strTrx = "KwikPayIns_" + EmployeeId;
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+
+            //...Command Interface...
+            SqlCommand cmdI = con.CreateCommand();
+            SqlTransaction trx;
+            // trx = con.BeginTransaction(strTrx);
+            cmdI.Connection = con;
+            cmdI.Transaction = trx;
+
+            try
+            {
+                //...Insert Record...
+                cmdI.CommandText = StoredProcedures.KwikPayTypeInsert;
+                cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+                //cmdI.Parameters.AddWithValue("@KwikPayID", ins.KwikPayID);             
+                cmdI.Parameters.AddWithValue("@KwikPayType", ins.KwikPayType);
+
+                //...Return new ID
+                ins.KwikPayTypeID = (int)cmdI.ExecuteScalar();
+
+                trx.Commit();
+                cmdI.Connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                if (trx != null) trx.Rollback();
+            }
+            finally
+            {
+                //Check for close and respond accordingly
+                if (con.State != ConnectionState.Closed)
+                {
+                    con.Close();
+                }
+                //Clean up
+                con.Dispose();
+                cmdI.Dispose();
+                trx.Dispose();
+            }
+            return ins;
+        }
+
+        public KwikPayType Update(KwikPayType ins)
+        {
+            //...Get User and Date Data...
+            // string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            // int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            //...Update Record...
+            cmdI.Parameters.Clear();
+            cmdI.CommandText = StoredProcedures.KwikPayTypeUpdate;
+            cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdI.Parameters.AddWithValue("@KwikPayTypeID", ins.KwikPayTypeID);
+            cmdI.Parameters.AddWithValue("@KwikPayType", ins.KwikPayType);
+
+            cmdI.ExecuteNonQuery();
+            cmdI.Connection.Close();
+
+            return ins;
+
+        }
+
+        public void Remove(int KwikPayTypeID)
+        {
+            //...Get User and Date Data...
+            //string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            //int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            //...Update Record...
+            cmdI.Parameters.Clear();
+            cmdI.CommandText = StoredProcedures.KwikPayTypeRemove;
+            cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdI.Parameters.AddWithValue("@KwikPayTypeID", KwikPayTypeID);
+
+            cmdI.ExecuteNonQuery();
+            cmdI.Connection.Close();
+        }
+
+        public void Remove(string KwikPayTypeIds)
+        {
+            List<int> RemoveIds = KwikPayTypeIds.Split(',').ToList().Select(int.Parse).ToList();
+
+            //...Get Date and Current User
+            //string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            //int UserId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            foreach (int ID in RemoveIds)
+            {
+                //...Remove Record...
+                cmdI.Parameters.Clear();
+                cmdI.CommandText = StoredProcedures.KwikPayRemove;
+                cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+                cmdI.Parameters.AddWithValue("@KwikPayTypeID", ID);
+                cmdI.ExecuteNonQuery();
+            }
+
+            cmdI.Connection.Close();
+        }  
     }
 }
--- SparStelsel/Models/MoneyUnitRepository.cs
+++ SparStelsel/Models/MoneyUnitRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,206 @@ namespace SparStelsel.Models
 {
     public class MoneyUnitRepository
     {
+        public MoneyUnit GetMoneyUnit(int MoneyUnitID)
+        {
+            //...Create New Instance of Object...
+            MoneyUnit ins = new MoneyUnit();
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            SqlCommand cmdI;
+
+            //...SQL Commands...
+            cmdI = new SqlCommand("SELECT * FROM l_MoneyUnit WHERE MoneyUnitID =" + MoneyUnitID, con);
+            cmdI.Connection.Open();
+            SqlDataReader drI = cmdI.ExecuteReader();
+
+            //...Retrieve Data...
+            if (drI.HasRows)
+            {
+                while (drI.Read())
+                {
+                    ins.MoneyUnitID = Convert.ToInt32(drI["MoneyUnitID"]);
+                    ins.MoneyUnit = Convert.ToChar(drI["MoneyUnit"]);
+                }
+            }
+
+            //...Close Connections...
+            drI.Close();
+            con.Close();
+
+
+            //...Return...
+            return ins;
+        }
+
+        public List<MoneyUnit> GetAllMoneyUnit()
+        {
+            //...Create New Instance of Object...
+            List<MoneyUnit> list = new List<MoneyUnit>();
+            MoneyUnit ins;
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            SqlCommand cmdI;
+
+            //...SQL Commands...
+            cmdI = new SqlCommand("SELECT * FROM l_MoneyUnit", con);
+            cmdI.Connection.Open();
+            SqlDataReader drI = cmdI.ExecuteReader();
+
+            //...Retrieve Data...
+            if (drI.HasRows)
+            {
+                while (drI.Read())
+                {
+                    ins = new MoneyUnit();
+                    ins.MoneyUnitID = Convert.ToInt32(drI["MoneyUnitID"]);
+                    ins.MoneyUnit = Convert.ToChar(drI["MoneyUnit"]);
+                    list.Add(ins);
+                }
+            }
+
+            //...Close Connections...
+            drI.Close();
+            con.Close();
+
+
+            //...Return...
+            return list;
+        }
+
+        public MoneyUnit Insert(MoneyUnit ins)
+        {
+            //...Get User and Date Data...
+            string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+            string strTrx = "MoneyUnitIns_" + EmployeeId;
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+
+            //...Command Interface...
+            SqlCommand cmdI = con.CreateCommand();
+            SqlTransaction trx;
+            trx = con.BeginTransaction(strTrx);
+            cmdI.Connection = con;
+            cmdI.Transaction = trx;
+
+            try
+            {
+                //...Insert Record...
+                cmdI.CommandText = StoredProcedures.MoneyUnitInsert;
+                cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+                //cmdI.Parameters.AddWithValue("@MoneyUnitID", ins.MoneyUnitID);             
+                cmdI.Parameters.AddWithValue("@MoneyUnit", ins.MoneyUnit);
+
+                //...Return new ID
+                ins.MoneyUnitID = (int)cmdI.ExecuteScalar();
+
+                trx.Commit();
+                cmdI.Connection.Close();
+            }
+            catch (SqlException ex)
+            {
+                if (trx != null) trx.Rollback();
+            }
+            finally
+            {
+                //Check for close and respond accordingly
+                if (con.State != ConnectionState.Closed)
+                {
+                    con.Close();
+                }
+                //Clean up
+                con.Dispose();
+                cmdI.Dispose();
+                trx.Dispose();
+            }
+            return ins;
+        }
+
+        public MoneyUnit Update(MoneyUnit ins)
+        {
+            //...Get User and Date Data...
+            string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            //...Update Record...
+            cmdI.Parameters.Clear();
+            cmdI.CommandText = StoredProcedures.MoneyUnitUpdate;
+            cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdI.Parameters.AddWithValue("@MoneyUnitID", ins.MoneyUnitID);
+            cmdI.Parameters.AddWithValue("@MoneyUnit", ins.MoneyUnit);
+
+
+            cmdI.ExecuteNonQuery();
+            cmdI.Connection.Close();
+
+            return ins;
+
+        }
+
+        public void Remove(int MoneyUnitID)
+        {
+            //...Get User and Date Data...
+            string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            int EmployeeId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            //...Update Record...
+            cmdI.Parameters.Clear();
+            cmdI.CommandText = StoredProcedures.MoneyUnitRemove;
+            cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdI.Parameters.AddWithValue("@MoneyUnitID", MoneyUnitID);
+
+            cmdI.ExecuteNonQuery();
+            cmdI.Connection.Close();
+        }
+
+        public void Remove(string MoneyUnitIds)
+        {
+            List<int> RemoveIds = MoneyUnitIds.Split(',').ToList().Select(int.Parse).ToList();
+
+            //...Get Date and Current User
+            string ModifiedDate = string.Format("{0:yyyy-MM-dd hh:mm:ss}", DateTime.Now);
+            int UserId = Convert.ToInt32(HttpContext.Current.Session["UserID"]);
+
+            //...Database Connection...
+            DataBaseConnection dbConn = new DataBaseConnection();
+            SqlConnection con = dbConn.SqlConn();
+            con.Open();
+            SqlCommand cmdI = con.CreateCommand();
+            cmdI.Connection = con;
+
+            foreach (int ID in RemoveIds)
+            {
+                //...Remove Record...
+                cmdI.Parameters.Clear();
+                cmdI.CommandText = StoredProcedures.MoneyUnitRemove;
+                cmdI.CommandType = System.Data.CommandType.StoredProcedure;
+                cmdI.Parameters.AddWithValue("@MoneyUnitID", ID);
+                cmdI.ExecuteNonQuery();
+            }
+
+            cmdI.Connection.Close();
+        }  
     }
 }
